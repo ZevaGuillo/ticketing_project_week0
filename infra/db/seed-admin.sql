75806314deadfe76779fae52832c7fb9ae1e0f72
ZevaGuillo <guiller.zeva16@gmail.com>
2026-03-20 12:57:20 -0500

    Add migration service and seed admin user to Docker Compose setup

---
+++ infra/db/seed-admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO bc_identity."Users" ("Id", "Email", "PasswordHash", "Role")
+VALUES ('4877cf29-81c5-49fb-9c5f-06c153fb9247'::uuid, 'admin@ticketing.com', '$2a$11$UpVfv.GnIe8BhDBHWIXic.nY04eN6NEpNlxw02/rJ/xMdio9zyd7.', 1);
